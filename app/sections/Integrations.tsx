9072521289b2e67fec9c8d0a7816f1b56b3898cb
NjilaBoris <njilaboris2@gmail.com>
2025-09-15 15:56:12 +0100

    integration V1

--- app/sections/Integrations.tsx
+++ app/sections/Integrations.tsx
@@ -1,12 +1,87 @@
+import Tag from "@/components/Tag";
+import figmaIcon from "@/public/images/figma-logo.svg";
+import notionIcon from "@/public/images/notion-logo.svg";
+import slackIcon from "@/public/images/slack-logo.svg";
+import relumeIcon from "@/public/images/relume-logo.svg";
+import framerIcon from "@/public/images/framer-logo.svg";
+import githubIcon from "@/public/images/github-logo.svg";
+import Image from "next/image";
+
 const integrations = [
-    { name: "Figma", icon: "figma-logo.svg", description: "Figma is a collaborative interface design tool." },
-    { name: "Notion", icon: "notion-logo.svg", description: "Notion is an all-in-one workspace for notes and docs." },
-    { name: "Slack", icon: "slack-logo.svg", description: "Slack is a powerful team communication platform." },
-    { name: "Relume", icon: "relume-logo.svg", description: "Relume is a no-code website builder and design system." },
-    { name: "Framer", icon: "framer-logo.svg", description: "Framer is a professional website prototyping tool." },
-    { name: "GitHub", icon: "github-logo.svg", description: "GitHub is the leading platform for code collaboration." },
+  {
+    name: "Figma",
+    icon: figmaIcon,
+    description: "Figma is a collaborative interface design tool.",
+  },
+  {
+    name: "Notion",
+    icon: notionIcon,
+    description: "Notion is an all-in-one workspace for notes and docs.",
+  },
+  {
+    name: "Slack",
+    icon: slackIcon,
+    description: "Slack is a powerful team communication platform.",
+  },
+  {
+    name: "Relume",
+    icon: relumeIcon,
+    description: "Relume is a no-code website builder and design system.",
+  },
+  {
+    name: "Framer",
+    icon: framerIcon,
+    description: "Framer is a professional website prototyping tool.",
+  },
+  {
+    name: "GitHub",
+    icon: githubIcon,
+    description: "GitHub is the leading platform for code collaboration.",
+  },
 ];
 
 export default function Integrations() {
-    return <div>Integrations</div>;
+  return (
+    <section className="py-24 overflow-clip">
+      <div className="container">
+        <Tag>Intergrations</Tag>
+        <h2 className="text-6xl font-medium mt-6">
+          Plays well with <span className="text-lime-400">others</span>
+        </h2>
+        <p className="text-white/50 mt-4 text-lg">
+          Layers seamlessly connects with your favorite tool making it easier to
+          plug into any workflow and collaborate accross platforms.
+        </p>
+        <div
+          className="h-[400px] mt-8 overflow-clip  [-webkit-mask-image:linear-gradient(to_bottom,rgba(0,0,0,0)_0%,rgba(0,0,0,1)_10%,rgba(0,0,0,1)_90%,rgba(0,0,0,0)_100%)]
+          [mask-image:linear-gradient(to_bottom,rgba(0,0,0,0)_0%,rgba(0,0,0,1)_10%,rgba(0,0,0,1)_90%,rgba(0,0,0,0)_100%)]
+          [-webkit-mask-repeat:no-repeat] [mask-repeat:no-repeat]
+          [-webkit-mask-size:100%_100%] [mask-size:100%_100%]"
+        >
+          <div className="flex flex-col gap-4 pb-4">
+            {integrations.map((integration) => (
+              <div
+                key={integration.name}
+                className="bg-neutral-900 border-white/10 rounded-3xl p-6"
+              >
+                <div className="flex justify-center">
+                  <Image
+                    src={integration.icon}
+                    alt={integration.name}
+                    className="size-24"
+                  />
+                </div>
+                <h3 className="text-3xl text-center mt-6">
+                  {integration.name}
+                </h3>
+                <p className="text-center text-white/50 mt-2">
+                  {integration.description}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
