6781afeae694cbfa30192b963c0e7080fbd7d39b
NjilaBoris <njilaboris2@gmail.com>
2025-09-15 13:49:35 +0100

    Features V2

--- app/sections/Features.tsx
+++ app/sections/Features.tsx
@@ -20,17 +20,18 @@ const features = [
 export default function Features() {
   return (
     <section className="py-24">
-      <div className="container">
+      <div className="container lg:pl-5">
         <div className="flex justify-center">
           <Tag>Features</Tag>
         </div>
-        <h2 className="text-6xl font-medium text-center mt-6">
+        <h2 className="text-6xl font-medium text-center mt-6 max-w-2xl mx-auto">
           Where power meets <span className="text-lime-400">simplicity</span>
         </h2>
-        <div className="mt-12 grid-cols-1 grid gap-8 px-5">
+        <div className="mt-12 grid-cols-1 lg:grid-cols-3 grid gap-8 px-5 md:grid-cols-4">
           <FeatureCard
             title="Real-time Collaboration"
             description="Work together seamlessly with conflict-free team editing"
+            className="md:col-span-2 lg:col-span-1"
           >
             <div className="aspect-video flex items-center justify-center">
               <Avatar className="z-40">
@@ -57,6 +58,7 @@ export default function Features() {
           <FeatureCard
             title="Interacting Prototyping"
             description=" Engage your clients with prototypes that react to user actions"
+            className="md:col-span-2 lg:col-span-1"
           >
             <div className="aspect-video flex items-center justify-center">
               <p className="text-4xl font-extrabold text-white/20 text-center">
@@ -69,6 +71,7 @@ export default function Features() {
             </div>
           </FeatureCard>
           <FeatureCard
+            className="md:col-span-2 md:col-start-2 lg:col-span-1 lg:col-start-auto"
             title="Keyboard Quick Actions"
             description="Powerful commands to help you create designs more quickly"
           >
@@ -79,11 +82,16 @@ export default function Features() {
             </div>
           </FeatureCard>
         </div>
-        <div>
+        <div className="mt-8 flex flex-wrap gap-3 pl-5 justify-center">
           {features.map((feature) => (
-            <div key={feature}>
-              <span></span>
-              <span>{feature}</span>
+            <div
+              key={feature}
+              className="bg-neutral-900 border-white/10 md:px-5 md:py-2 inline-flex px-3 py-1.5 gap-3 items-center rounded-2xl"
+            >
+              <span className="bg-lime-400 text-xl text-neutral-950 size-5 inline-flex items-center justify-center rounded-full">
+                &#10038;
+              </span>
+              <span className="font-medium md:text-lg">{feature}</span>
             </div>
           ))}
         </div>
