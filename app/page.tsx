1b4a9375f7e2e47565c3c1912dfba04c81e294ca
NjilaBoris <njilaboris2@gmail.com>
2025-09-16 03:30:25 +0100

    Faqs V2

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Introduction from "./sections/Introduction";
 import LogoTicker from "./sections/LogoTicker";
 import Navbar from "./sections/Navbar";
 import Faqs from "./sections/Faqs";
+import CallToAction from "./sections/CallToAction";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Features />
         <Integrations />
         <Faqs />
+        <CallToAction />
       </div>
     </ReactLenis>
   );
